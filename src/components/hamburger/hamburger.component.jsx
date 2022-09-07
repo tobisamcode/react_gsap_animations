58c9c33e3fbb64d80722b637485bec506443dd2c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-07 23:01:22 +0100

    react-animations

---
+++ src/components/hamburger/hamburger.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Hamburger = () => {
+  return (
+    <div className="hamburger-menu">
+      <div className="menu-secondary-background-color"></div>
+      <div className="menu-layer">
+        <div className="menu-city-background"></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/opportunities">Opportunities</Link>
+                  </li>
+                  <li>
+                    <Link to="/solutions">Solutions</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact-us">Contact</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;
