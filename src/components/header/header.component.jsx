9a502f1f86023d536ac4b91c952941b203f3af52
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-07 22:49:35 +0100

    react-animations

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">HAMBRG.</Link>
+            </div>
+            <div className="menu">
+              <button>Menu</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
