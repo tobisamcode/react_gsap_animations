fc82748fa788d56c6d4faa1f98f0536699f65a5d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-07 22:42:34 +0100

    react-animations

--- src/App.js
+++ src/App.js
@@ -2,44 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
+import Contact from "./pages/contact/contact.component";
+import Home from "./pages/home/home.component";
+import Opportunities from "./pages/opportunities/opportunities.component";
+import Solution from "./pages/solution/solution.component";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={Home} />
-        <Route exact path="/opportunities" element={Opportunities} />
-        <Route exact path="/solutions" element={Solutions} />
-        <Route exact path="/contact-us" element={Contact} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/opportunities" element={<Opportunities />} />
+        <Route exact path="/solutions" element={<Solution />} />
+        <Route exact path="/contact-us" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
 };
-
-function Opportunities() {
-  return <p>Discover our numerous opportunities</p>;
-}
-
-function Solutions() {
-  return <p>Solutions that help you.</p>;
-}
-
-function Contact() {
-  return <p>Feel free to reach us.</p>;
-}
-
-function Home() {
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <h5>
-          The <b>HAMBRG</b>, is a creative, engineer driven, global agency
-          working on advancing the software, advertising and design communities
-          to new heights.
-        </h5>
-      </div>
-    </div>
-  );
-}
 
 export default App;
