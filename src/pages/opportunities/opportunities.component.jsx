df0b54c1267a025f7f2599ab08f8486de1564cec
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-07 22:42:59 +0100

    react-animations

---
+++ src/pages/opportunities/opportunities.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Opportunities = () => {
+  return (
+    <div>
+      <p>Discover our numerous opportunities</p>;
+    </div>
+  );
+};
+
+export default Opportunities;
