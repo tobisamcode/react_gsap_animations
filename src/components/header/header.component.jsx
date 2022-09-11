85db2692afcb6902bd9c20d98edd4371e253d508
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-11 19:21:17 +0100

    react-animations

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import Hamburger from "../hamburger/hamburger.component";
 
 const Header = () => {
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu"
+  });
+
   return (
     <header>
       <div className="container">
